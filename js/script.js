41868b6775c4e1abf56386c5495e0df913b39f94
Marcelo Inácio <103380466+MarceloInacioJr@users.noreply.github.com>
2023-02-23 23:13:34 -0300

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const toMove =  document.querySelector(".cover-tomove");
+
+
+
+
+// fuctions
+function toMoveCover() {
+ let a = document.querySelector(".image-cover")
+  a.classList.toggle("toMoveCover");
+}
